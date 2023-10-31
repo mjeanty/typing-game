1ee4237ce6051219647919693b718097b2f5786d
maxjeanty <maxjeanty@gmail.com>
2023-10-31 00:17:12 -0500

    started JS

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const word  = document.getElementById( ' word' ),
+      text = document.getElementById( ' text' ),
+      scoreEl = document.getElementById( ' score' ),
+      timeEl = document.getElementById( ' time ' ),
+      endgameEl =  document.getElementById( ' end-game  ' ),
+      settingsBtn =document.getElementById( ' settings-btn ' ),
+      settings = document.getElementById( ' settings ' ),
+      settingsForm = document.getElementById( ' settings-form ' ),
+      difficultySelect = document.getElementById( ' difficulty ' )
+
+
