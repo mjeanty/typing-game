2ffaef73e635ef35946ad75bc665f7e03211bcab
maxjeanty <maxjeanty@gmail.com>
2023-10-31 22:51:57 -0500

    adde input funtionality and focus functionality

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const word  = document.getElementById( ' word' )
-      text = document.getElementById( ' text' )
-      scoreEl = document.getElementById( ' score' )
-      timeEl = document.getElementById( ' time ' )
-     endgameEl =  document.getElementById( ' end-game  ' )
-      settingsBtn =document.getElementById( ' settings-btn ' )
-      settings = document.getElementById( ' settings ' )
-      settingsForm = document.getElementById( ' settings-form ' )
-      difficultySelect = document.getElementById( ' difficulty ' )
+const word  = document.getElementById( 'word' )
+const      text = document.getElementById( 'text' )
+const      scoreEl = document.getElementById( 'score' )
+const      timeEl = document.getElementById( 'time' )
+const     endgameEl =  document.getElementById( 'end-game' )
+const      settingsBtn =document.getElementById( 'settings-btn' )
+const      settings = document.getElementById( 'settings' )
+const      settingsForm = document.getElementById( 'settings-form' )
+const      difficultySelect = document.getElementById( 'difficulty' )
 
 
 const words = [
@@ -28,8 +28,34 @@ let score = 0;
 ///initialize time
 let time = 10;
 
-function getRandomWord(){
-    return words[Math.floor(Math.random() * words.length)]
-   
+text.focus()
+
+
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)]
 }
- 
+
+// add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// update score
+function updateScore (){
+  score++
+  scoreEl.innerHTML = score
+}
+
+addWordToDOM()
+
+
+// event listeners
+text.addEventListener('input', e => {
+  const insertedText = e.target.value
+  if(insertedText === randomWord){
+   addWordToDOM()
+   updateScore()
+   e.target.value = ""
+  }
+})
